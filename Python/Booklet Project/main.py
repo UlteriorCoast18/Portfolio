ac216f450a35645eb07d9f9ffbef2cc94e0bcdaa
UlteriorCoast18 <cristo.daniel.alvarado@gmail.com>
2025-02-25 22:25:35 -0600

    Avances Booklet Project

--- Python/Booklet Project/main.py
+++ Python/Booklet Project/main.py
@@ -1,23 +1,23 @@
 import tkinter as tk
+from tkinter import filedialog, ttk
+from PIL import Image, ImageTk
 import pdf_methods as pm
+import os
 
-window_height = 900
-window_width = 1400
+window_height = 1000
+window_width = 1600
+
+#Image Escale
+image_escale = 4
 
 #Número que determina número de páginas de un booklet, ideal entre 6 y 10 
 size_booklet = 6
-
+file_path = ""
 class App(tk.Frame):
     def __init__(self, master):
         super().__init__(master)
-        self.pack()
-
-        #Create Objects within the frame
-
-        self.label_1 = tk.Label(text = "Hello")
-        self.label_1.place(relx=0.5, rely=0.5, anchor='center')
-        self.label_1.pack()
-        
+        self.master = master
+        self.create_widgets()
 
         """
         self.entrythingy = tk.Entry()
@@ -35,24 +35,94 @@ class App(tk.Frame):
         self.entrythingy.bind('<Key-Return>', self.print_contents)
         """
 
-    def print_contents(self, event):
-        print("Hi. The current entry content is:", self.contents.get())
+    def create_widgets(self):
+        #Button choose file
+        self.direct_button = tk.Button(self, text="File Directory", command=self.open_file_dialog)
+        self.direct_button.grid(row=1, column=0, sticky="nsew", padx=5, pady=5)
+        
+        #File Directory
+        self.direct_entry = tk.Label(self, text="Vacío")
+        self.direct_entry.grid(row=1, column=1, sticky="nsew", padx=5, pady=5)
+
+        #Operations Label
+        self.label_operations = tk.Label(self, text="Options")
+        self.label_operations.grid(row=2, column=1, sticky="nsew", padx=5, pady=5)
+
+        #Crop Button
+        self.crop_button = tk.Button(self, text="Crop PDF")
+        self.crop_button.grid(row=3, column=1, sticky="nsew", padx=5, pady=5)
+        
+        #Prepare to Booklet PrintJob
+        self.booklet_print = tk.Button(self, text="Prepare PDF to Booklet")
+        self.booklet_print.grid(row=4, column=1, sticky="nsew", padx=5, pady=5)
+
+        #Resize PDF
+        self.resize_pdf = tk.Button(self, text="Resize PDF")
+        self.resize_pdf.grid(row=5, column=1, sticky="nsew", padx=5, pady=5)
+
+        #Select size PDF
+        self.combo_size = ttk.Combobox(
+            self,
+            state="readonly",
+            values = ["A1","A2","A3","A4","A5","A6"]
+        )
+        self.combo_size.grid(row=5, column=1, sticky="nsew", padx=5, pady=5)
+
+        #PDF Icon
+        self.icon_size = tk.Label(self)
+        self.icon_size.grid(row=2, column=0, padx=5, pady=5, rowspan=10)
+        self.change_image(os.getcwd() + '/images/placeholder_pdf.png')
+        
+    def open_file_dialog(self):
+        file_path = filedialog.askopenfilename(initialdir="/home/cristoalvarado/Documents/Books To Print/", title="Select a PDF", filetypes=[("PDF", "*.pdf"), ("All files", "*.*")])
+        if file_path:
+            self.direct_entry.config(text = f"{file_path}")
+            self.show_pdf(file_path)
+
+    def show_pdf(self, file_path):
+        png_path = os.getcwd()+'/aux/aux.png'
+        pm.showcase_pdf(file_path, png_path)
+
+        #Change frame image
+        self.change_image(png_path)
+
+    def show_pdf_to_crop(self, file_path):
+        aux_path = os.getcwd()+'/aux/aux.pdf'
+        png_path = os.getcwd()+'/aux/aux.png'
+        pm.overlay_pdf(file_path,aux_path)
+        pm.pdf_to_png(aux_path,png_path)
+
+        #Change frame image
+        self.change_image(png_path)
+
+    def change_image(self, png_path):
+        self.img = Image.open(png_path)
+        self.img = self.img.resize((image_escale *148,image_escale *210))
+        self.img = ImageTk.PhotoImage(self.img)
+        self.icon_size.configure(image=self.img)
+
+
+    def refresh(self):
+        self.destroy()
+        self.__init__(self.master)
 
 
 if __name__ == "__main__":
     #Window configuration
-    """
     root = tk.Tk()
     screen_height = root.winfo_screenheight()
     screen_width = root.winfo_screenwidth()
     root.geometry(f"{window_width}x{window_height}+{int((screen_width - window_width)/2)}+{int((screen_height-window_height)/2)}")
     root.title("Python Booklet Project")
     root.positionfrom()
-    
+
     #Window Init
     myapp = App(root)
-    myapp.mainloop()
-    """
-    route = '/home/cristoalvarado/Documents/Books To Print/Libro Mau 1/test_1.pdf'
-    output = '/home/cristoalvarado/Documents/Books To Print/Libro Mau 1/output.pdf'
-    pm.resize_pdf(route,output,size_booklet)
+    myapp.grid(row=1, column=1)
+    myapp.columnconfigure(0, weight=1)
+    myapp.columnconfigure(1, weight=2)
+    root.mainloop()
+
+    #route = '/home/cristoalvarado/Documents/Books To Print/Libro Mau 1/Libro Mau 1.pdf'
+    #output = '/home/cristoalvarado/Documents/Books To Print/Libro Mau 1/output.pdf'
+    #pm.prepare_print(route,output,size_booklet)
--- Python/Booklet Project/pdf_methods.py
+++ Python/Booklet Project/pdf_methods.py
@@ -1,7 +1,10 @@
 from PyPDF2 import PdfReader, PdfWriter, Transformation, PageObject, PaperSize
 from PyPDF2.generic import RectangleObject
+from pdf2image import convert_from_path
+from os import remove
+import os
 
-def resize_pdf(pdf_path, output_path, size_booklet):
+def resize_pdf_A5(pdf_path, output_path, add_pages):
     pdf_reader = PdfReader(pdf_path)
     writer = PdfWriter()
 
@@ -33,30 +36,137 @@ def resize_pdf(pdf_path, output_path, size_booklet):
 
         #Show completion
         print(str(int(100*i/num_pages))+'%')
+    
+    #In case we need to add some more pages
+
+    if add_pages:
+        print('Adding pages')
+        num_pages = num_pages % 4
+        while num_pages % 4 != 0:
+            A5_w = PaperSize.A5.width
+            A5_h = PaperSize.A5.height
+            page_A5 = PageObject.create_blank_page(width = A5_w, height = A5_h)
+            writer.add_page(page_A5)
+            num_pages += 1
+
     writer.write(output_path)
-
-
-    #Obtenemos el número de páginas
-    #num_pages = 
-
-    #Reescalamos todas las páginas del pdf
-    """
-    for page in pdf_reader.pages:
-        page.scale_by(2)
-        writer.add_page(page)
-    """
-    #Operamos con el número de páginas para saber cuantos cuadernillos hacer    
-    
-    #Número total de cuadernillos
-    #num_booklets = int(num_pages/(size_booklet*4)) + 1
-    #Páginas restantes en el último cuadernillo
-    #num_rest = num_pages - int(num_pages/(size_booklet*4))*(size_booklet*4)
-    
-    #array_pages = array_pages(num_booklets,num_rest)
-
-    print('finished')
-
+    print('Finished reescaling and/or adding pages')
     return 0;
 
-def array_pages(num_booklets,num_rest):
-    return 0;
+def prepare_print(pdf_path, output_path, size_booklet):
+    #Auxiliar path to generate resized and correct number of pages for pdf
+    aux_path = '/home/cristoalvarado/aux_1.pdf'
+    resize_pdf_A5(pdf_path, aux_path, 1)
+    
+    #Now we open auxiliar pdf with extra pages
+    pdf_reader = PdfReader(aux_path)
+    
+    #Number of pages to generate booklet
+    num_pages = len(pdf_reader.pages)
+
+    #Obtain rearranged pages
+    arranged_pages = array_pages(num_pages,size_booklet)
+
+    #Now we create final PDF ready to print
+    writer = PdfWriter()
+    A4_w = PaperSize.A4.width
+    A4_h = PaperSize.A4.height
+    for i in range(int(len(arranged_pages)/2)):
+        page_A4 = PageObject.create_blank_page(width = A4_w, height = A4_h)
+        page_1 = pdf_reader.pages[arranged_pages[2*i] - 1]
+        page_2 = pdf_reader.pages[arranged_pages[2*i + 1] - 1]
+        page_1.mediabox = page_A4.mediabox
+        page_2.mediabox = page_A4.mediabox
+        #Odd page, rotate clokwise 90°
+        if i % 2 == 1:
+            page_1.add_transformation(Transformation().rotate(90))
+            page_1.add_transformation(Transformation().translate(A4_w,A4_h/2))
+            page_2.add_transformation(Transformation().rotate(90))
+            page_2.add_transformation(Transformation().translate(A4_w,0))
+        #Even page, rotate clokwise -90°
+        else:
+            page_2.add_transformation(Transformation().rotate(-90))
+            page_2.add_transformation(Transformation().translate(0,A4_h))
+            page_1.add_transformation(Transformation().rotate(-90))
+            page_1.add_transformation(Transformation().translate(0,A4_h/2))
+        #Now merge pages in page_A4
+        page_A4.merge_page(page_1)
+        page_A4.merge_page(page_2)
+        writer.add_page(page_A4)
+        print(str(int(i/int(len(arranged_pages)/2)*100))+'%')
+    writer.write(output_path)
+    remove(aux_path)
+    print('Finished Booklet')
+
+def overlay_pdf(pdf_path, output_path):
+    #Auxiliar path to generate overlaped pdf file
+    aux_path = '/home/cristoalvarado/aux_2.pdf'
+    resize_pdf_A5(pdf_path, aux_path, 0)
+
+    pdf_reader = PdfReader(aux_path)
+    writer = PdfWriter()
+    num_pages = len(pdf_reader.pages)
+    
+    A5_w = PaperSize.A5.width
+    A5_h = PaperSize.A5.height
+    
+    page_A5 = PageObject.create_blank_page(width = A5_w, height = A5_h)
+
+    for i in range(min(num_pages,40)):
+        if i > 5:
+            page = pdf_reader.pages[i]
+            page_A5.merge_page(page)
+
+    writer.add_page(page_A5)
+    writer.write(output_path)
+    remove(aux_path)
+    print('Finished Overlay')
+
+def showcase_pdf(pdf_path,output_path):
+    #Output path is a png image and pdf_path is the original pdf
+    pdf_reader = PdfReader(pdf_path)
+    aux_path = os.getcwd()+'/aux/aux.pdf'
+
+    #Obtain and generate a pdf with only the first page
+    writer = PdfWriter()
+    writer.add_page(page=pdf_reader.pages[0])
+    writer.write(aux_path)
+    
+    #Convert pdf to png
+    pdf_to_png(aux_path,output_path)
+
+def pdf_to_png(pdf_path,output_path):
+    images = convert_from_path(pdf_path)
+    for img in images:
+        img.save(output_path, 'PNG')
+    remove(pdf_path)
+
+def array_pages(num_pages,size_booklet):
+    #Obtain number of booklets
+    num_booklets = int(num_pages/(size_booklet*4)) + 1
+    arranged_pages = []
+    i = 0
+    rearranged_booklets = 0
+    #Rearrange as usual
+    while num_booklets > 0:
+        #If booklet is big enough
+        if num_booklets != 1:
+            for i in range(size_booklet):
+                arranged_pages.append(rearranged_booklets + 1 + 2*i)
+                arranged_pages.append(rearranged_booklets + 4*size_booklet - 2*i)
+                arranged_pages.append(rearranged_booklets + 4*size_booklet - 1 - 2*i)
+                arranged_pages.append(rearranged_booklets + 2 + 2*i)
+            rearranged_booklets += 4*size_booklet
+        #If booklet is too small
+        else:
+            #See where it got
+            resting_number = num_pages - rearranged_booklets
+            for i in range(int(resting_number/4)):
+                arranged_pages.append(rearranged_booklets + 1 + 2*i)
+                arranged_pages.append(rearranged_booklets + resting_number - 2*i)
+                arranged_pages.append(rearranged_booklets + resting_number - 1 - 2*i)
+                arranged_pages.append(rearranged_booklets + 2 + 2*i)
+            rearranged_booklets += resting_number
+        num_booklets -= 1
+    print('Rearranged pages: '+str(rearranged_booklets))
+    return arranged_pages
