086f9427e21c8184c652ae69fa526cca93bf7b89
UlteriorCoast18 <cristo.daniel.alvarado@gmail.com>
2025-02-26 14:33:05 -0600

    Avances Booklet Project 26/02

--- Python/Booklet Project/main.py
+++ Python/Booklet Project/main.py
@@ -7,12 +7,18 @@ import os
 window_height = 1000
 window_width = 1600
 
+click_num = 0
+x1, y1 ,x2, y2 = 0, 0, 0, 0
+
+pdf_selected = False
+
 #Image Escale
 image_escale = 4
 
 #Número que determina número de páginas de un booklet, ideal entre 6 y 10 
 size_booklet = 6
 file_path = ""
+
 class App(tk.Frame):
     def __init__(self, master):
         super().__init__(master)
@@ -49,7 +55,7 @@ class App(tk.Frame):
         self.label_operations.grid(row=2, column=1, sticky="nsew", padx=5, pady=5)
 
         #Crop Button
-        self.crop_button = tk.Button(self, text="Crop PDF")
+        self.crop_button = tk.Button(self, text="Start Cropping PDF", command=self.show_pdf_to_crop)
         self.crop_button.grid(row=3, column=1, sticky="nsew", padx=5, pady=5)
         
         #Prepare to Booklet PrintJob
@@ -68,10 +74,18 @@ class App(tk.Frame):
         )
         self.combo_size.grid(row=5, column=1, sticky="nsew", padx=5, pady=5)
 
-        #PDF Icon
+        #Canva Draw
+        self.pdf_display = tk.Canvas(self, width=image_escale *148, height=image_escale *210, background="white")
+        self.pdf_display.grid(row=2, column=0, padx=5, pady=5, rowspan=10)
+        self.pdf_display.bind('<ButtonPress-1>', self.draw_line)
+        self.pdf_display.bind('<ButtonRelease-1>', self.end_draw)
+        self.pdf_display.bind("<Motion>", self.update_position)
         self.icon_size = tk.Label(self)
-        self.icon_size.grid(row=2, column=0, padx=5, pady=5, rowspan=10)
         self.change_image(os.getcwd() + '/images/placeholder_pdf.png')
+
+        #Clear Canva
+        self.clea_pdf_display = tk.Button(self, text='Clear Lines', command=lambda: self.pdf_display.delete('lines'))
+        self.clea_pdf_display.grid(row=6, column=1, sticky="nsew", padx=5, pady=5)
         
     def open_file_dialog(self):
         file_path = filedialog.askopenfilename(initialdir="/home/cristoalvarado/Documents/Books To Print/", title="Select a PDF", filetypes=[("PDF", "*.pdf"), ("All files", "*.*")])
@@ -86,7 +100,10 @@ class App(tk.Frame):
         #Change frame image
         self.change_image(png_path)
 
-    def show_pdf_to_crop(self, file_path):
+    def show_pdf_to_crop(self):
+        file_path=self.direct_entry.cget('text')
+        global pdf_selected
+        pdf_selected=True
         aux_path = os.getcwd()+'/aux/aux.pdf'
         png_path = os.getcwd()+'/aux/aux.png'
         pm.overlay_pdf(file_path,aux_path)
@@ -99,15 +116,46 @@ class App(tk.Frame):
         self.img = Image.open(png_path)
         self.img = self.img.resize((image_escale *148,image_escale *210))
         self.img = ImageTk.PhotoImage(self.img)
-        self.icon_size.configure(image=self.img)
-
+        self.pdf_display.delete('all')
+        self.pdf_display.create_image(0,0, anchor = 'nw', image = self.img)
 
     def refresh(self):
         self.destroy()
         self.__init__(self.master)
 
+    def draw_line(self, event):
+        global pdf_selected
+        global click_num
+        global x1, y1
+        if click_num == 0 and pdf_selected:
+            self.pdf_display.delete('lines')
+            x1 = event.x
+            y1 = event.y
+            click_num = 1
+
+    def update_position(self, event):
+        global pdf_selected
+        global click_num
+        global x1, y1, x2, y2
+        if click_num == 1 and pdf_selected:
+            x2 = event.x
+            y2 = event.y
+            if x2 < 0: x2 = 0
+            if x2 > image_escale *148: x2 = image_escale *148
+            if y2 < 0: y2 = 0
+            if y2 > image_escale *210: y2 = image_escale *210
+            self.pdf_display.delete('lines')
+            self.pdf_display.create_rectangle(x1, y1, x2, y2, fill="lightblue", width=3, tags='lines', outline='#2bebf9', stipple='gray50')
+
+    def end_draw(self, event):
+        global pdf_selected
+        if pdf_selected:
+            global click_num
+            click_num = 0
+        
 
 if __name__ == "__main__":
+    
     #Window configuration
     root = tk.Tk()
     screen_height = root.winfo_screenheight()
@@ -122,7 +170,3 @@ if __name__ == "__main__":
     myapp.columnconfigure(0, weight=1)
     myapp.columnconfigure(1, weight=2)
     root.mainloop()
-
-    #route = '/home/cristoalvarado/Documents/Books To Print/Libro Mau 1/Libro Mau 1.pdf'
-    #output = '/home/cristoalvarado/Documents/Books To Print/Libro Mau 1/output.pdf'
-    #pm.prepare_print(route,output,size_booklet)
